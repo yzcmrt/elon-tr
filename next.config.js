414305e366c2c5e86ce0339570c4b84624f2cdec
Mert Yazici <yazici.mert.35@gmail.com>
2024-12-26 00:44:26 +0300

    Removed .ts config files from git tracking

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Konfigürasyon ayarları buraya
+}
+
+module.exports = nextConfig 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,4 +14,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+} 
